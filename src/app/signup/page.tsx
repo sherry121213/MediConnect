a43761410eea1cc3f9f84457b748f59acc83a5f1
Falak Ali <falakali1470@gmail.com>
2026-01-27 13:24:03 +0000

    also add check on password maximum 32  characters can be add as a passwo

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -29,7 +29,7 @@ const signupSchema = z.object({
   firstName: z.string().min(2, { message: 'First name must be at least 2 characters.' }).regex(/^[^\d]*$/, { message: "First name cannot contain numbers." }),
   lastName: z.string().min(2, { message: 'Last name must be at least 2 characters.' }).regex(/^[^\d]*$/, { message: "Last name cannot contain numbers." }),
   email: z.string().email({ message: 'Please enter a valid email address.' }),
-  password: z.string().min(6, { message: 'Password must be at least 6 characters.' }),
+  password: z.string().min(6, { message: 'Password must be at least 6 characters.' }).max(32, { message: 'Password cannot exceed 32 characters.' }),
   role: z.enum(['patient', 'doctor']),
 });
 
