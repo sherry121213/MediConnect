7beca817f9a1e9ff6bcc1ff19808aa6c10c19ecd
Falak Ali <falakali1470@gmail.com>
2026-05-11 20:20:23 +0000

    Implement this changes Make  "Storage Bucket" in Backend to store conten

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:138026146520:web:cec1d25427a52b148255ad",
   "apiKey": "AIzaSyD_PJJ7u2ggIyd5RoaElLf6z3mcMfXNR84",
   "authDomain": "studio-8733723883-a0e14.firebaseapp.com",
+  "storageBucket": "studio-8733723883-a0e14.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "138026146520"
 };
